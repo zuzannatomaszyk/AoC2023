eeb8485b922b9e34442def26321faf09532f69f4
Zuzanna Tomaszyk <zuzanna.tomaszyk@gmail.com>
2023-12-05 23:27:00 +0100

    day 5 part 2 bruteforce mess

--- 5/index.js
+++ 5/index.js
@@ -27,52 +27,42 @@ lines.forEach((line) => {
 })
 maps.push({ ...currentMap, ranges: [...ranges] });
 
-let allMapped = seeds.map((seed) => {
-    return {
-        seed,
-        soil: '',
-        fertilizer: '',
-        water: '',
-        light: '',
-        temperature: '',
-        humidity: '',
-        location: '',
-    }
-})
+// let allMapped = seeds.map((seed) => {
+//     return {
+//         seed,
+//         soil: '',
+//         fertilizer: '',
+//         water: '',
+//         light: '',
+//         temperature: '',
+//         humidity: '',
+//         location: '',
+//     }
+// })
 
-//SEEDS PART 2
-const seedsRanges = lines[0].slice(7).split(' ').map(Number);
 
-for (let i = 0; i < seedsRanges.length; i += 2) {
-    const start = seedsRanges[i];
-    const length = seedsRanges[i + 1];
+let minLoc = Infinity;
+for (let i = 0; i < seeds.length; i += 2) {
+    console.log(i);
+    const start = seeds[i];
+    const length = seeds[i + 1];
     for (let j = 0; j < length; j++) {
-        seeds.push(start + j);
+        let currentFROM = start + j;
+        maps.forEach((map) => {
+            for (const range of map.ranges) {
+                if (range.from <= currentFROM && currentFROM < (range.from + range.length)) {
+                    currentFROM = range.to + (currentFROM - range.from);
+                    break;
+                }
+            }
+        })
+        minLoc = Math.min(currentFROM, minLoc);
     }
 }
 
-maps.forEach((map) => {
-    const [FROM, TO] = map.name.split('-to-');
+// const minLoc = allMapped.reduce((acc, current) => {
+//     return Math.min(current.location, acc);
+// }, Infinity);
 
-    allMapped.forEach((element) => {
-        map.ranges.forEach((range) => {
-            if (range.from <= element[FROM] && element[FROM] < (range.from + range.length)) {
-                element[TO] = range.to + (element[FROM] - range.from);
-            }
-        })
-        if (element[TO] === '') {
-            element[TO] = element[FROM];
-        }
-    })
-})
-
-// Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-// Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-// Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-// Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
-
-const minLoc = allMapped.reduce((acc, current) => {
-    return Math.min(current.location, acc);
-}, Infinity);
 
 console.log(minLoc);
